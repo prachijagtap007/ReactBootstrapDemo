bd792dad69a4f95ac8dc50d1d7253635d649939b
Prachi Jagtap <Prachi.Jagtap@cytel.pune.in.local>
2020-11-24 19:52:48 +0530

    intial commit with homepage

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -6,27 +6,58 @@ import ToolkitProvider, { ColumnToggle } from 'react-bootstrap-table2-toolkit';
 export default function MyTable() {
 
 const { ToggleList } = ColumnToggle;  
-const products = [ {id: 101, name: "Chips", price:200},{id: 102, name: "Chips", price:200}];
+const products = [ {id: 101, name: "East", TokensGranted:15,TokenDenied:5, NoOfUsers:20,UsageTime:1200, FromDate: "12-01-2019", ToDate:"12-01-2020", Comment:"new company"},
+{id: 102, name: "Enforesys",TokensGranted:15,TokenDenied:5, NoOfUsers:20,UsageTime:1200, FromDate: "12-01-2019", ToDate:"12-01-2020", Comment:"new company"},
+{id: 103, name: "Solara", TokensGranted:15,TokenDenied:5, NoOfUsers:20,UsageTime:1200, FromDate: "12-01-2019", ToDate:"12-01-2020", Comment:"new company"},
+{id: 104, name: "Compass",TokensGranted:15,TokenDenied:5, NoOfUsers:20,UsageTime:1200, FromDate: "12-01-2019", ToDate:"12-01-2020", Comment:"new company"}, 
+{id: 105, name: "PTS", TokensGranted:15,TokenDenied:5, NoOfUsers:20,UsageTime:1200, FromDate: "12-01-2019", ToDate:"12-01-2020", Comment:"new company"},
+{id: 106, name: "East", TokensGranted:15,TokenDenied:5, NoOfUsers:20,UsageTime:1200, FromDate: "12-01-2019", ToDate:"12-01-2020", Comment:"new company"},
+{id: 107, name: "Enforesys",TokensGranted:15,TokenDenied:5, NoOfUsers:20,UsageTime:1200, FromDate: "12-01-2019", ToDate:"12-01-2020", Comment:"new company"},
+{id: 108, name: "Solara", TokensGranted:15,TokenDenied:5, NoOfUsers:20,UsageTime:1200, FromDate: "12-01-2019", ToDate:"12-01-2020", Comment:"new company"},
+{id: 109, name: "Compass",TokensGranted:15,TokenDenied:5, NoOfUsers:20,UsageTime:1200, FromDate: "12-01-2019", ToDate:"12-01-2020", Comment:"new company"}, 
+{id: 110, name: "PTS", TokensGranted:15,TokenDenied:5, NoOfUsers:20,UsageTime:1200, FromDate: "12-01-2019", ToDate:"12-01-2020", Comment:"new company"}];
 const columns = [{
     dataField: 'id',
-    text: 'Product ID',
+    text: 'Client ID',
     sort: true
   }, {
     dataField: 'name',
-    text: 'Product Name',
+    text: 'Software Name'
+  }, {
+    dataField: 'TokensGranted',
+    text: 'No of licenses Granted',
+    sort: true
+  },{
+    dataField: 'TokenDenied',
+    text: 'No of licenses Denied',
     sort: true
   }, {
-    dataField: 'price',
-    text: 'Product Price',
+    dataField: 'NoOfUsers',
+    text: 'No of users',
     sort: true
+  }, {
+    dataField: 'UsageTime',
+    text: 'Useage time (mins)',
+    sort: true
+  },{
+    dataField: 'FromDate',
+    text: 'From date',
+    sort: true
+  }, {
+    dataField: 'ToDate',
+    text: 'To date',
+    sort: true
+  }, {
+    dataField: 'Comment',
+    text: 'Comment'
   }];
   
   const defaultSorted = [{
     dataField: 'name',
     order: 'desc'
-  }];
+  },];
   
-const CaptionElement = () => <h3 style={{ borderRadius: '0.25em', textAlign:'center', color:'gray', border: '1px solid gray', padding: '0.5em' }}>React bootstrap table next</h3>;
+const CaptionElement = () => <h5 style={{ borderRadius: '0.25em', textAlign:'center', color:'gray', border: '1px solid gray', padding: '0.5em' }}>Software usage Report</h5>;
 
 return (<div className="container">
 <a href="https://openbase.io/js/react-bootstrap-table-next" target="_blank">Check package performance</a>
